798712e047c8dc054984360539a176c8eaa8b73b
enzo franceschini <enzo.franceschini@90tech.fr>
2022-08-22 21:30:32 +0200

    Add: maj

--- src/modules/apis/UserAPI.js
+++ src/modules/apis/UserAPI.js
@@ -1,21 +1,46 @@
-import * as UtilsAPI from './UtilsAPI';
+import * as UtilsAPI from "./UtilsAPI";
+import axios from "axios";
 
-export const getAllUser = (token,success,error) => {
-    return UtilsAPI.getAll('user',token,success,error);
-}
+const BASE_URL = "https://localhost:8000/api";
 
-export const getUser = (userId,token,success,error) => {
-    return UtilsAPI.get('user',token,userId,success,error);
-}
+export const getAllUser = (token, success, error) => {
+  return UtilsAPI.getAll("user", token, success, error);
+};
 
-export const postUser = (token,data,success,error) => {
-    return UtilsAPI.post('user',token,data,success,error);
-}
+export const getUser = (userId, token, success, error) => {
+  return UtilsAPI.get("user", token, userId, success, error);
+};
 
-export const patchUser = (token,userId,data,success,error) => {
-    return UtilsAPI.patch('user',token,userId,data,success,error);
-}
+export const postUser = (token, data, success, error) => {
+  return UtilsAPI.post("user", token, data, success, error);
+};
 
-export const removeUser = (token,userId,success,error) => {
-    return UtilsAPI.remove('user',token,userId,success,error);
-}
+export const patchUser = (token, userId, data, success, error) => {
+  return UtilsAPI.patch("user", token, userId, data, success, error);
+};
+
+export const removeUser = (token, userId, success, error) => {
+  return UtilsAPI.remove("user", token, userId, success, error);
+};
+
+export const login = (username, password, success, error) => {
+  axios
+    .post(`${BASE_URL}/login`, { username, password })
+    .then((response) => {
+      if (success) success(response.data);
+    })
+    .catch((err) => {
+      if (error) error(err.response.data);
+    });
+};
+
+export const getMyUser = (token, success, error) => {
+  axios
+    .get(`${BASE_URL}/user/me`, UtilsAPI.getTokenHeader(token))
+    .then((response) => {
+      if (success) success(response.data);
+    })
+    .catch((err) => {
+      if (error) error(err.response.data);
+    });
+};
--- src/pages/contact/ContactAutre/ContactAutre.js
+++ src/pages/contact/ContactAutre/ContactAutre.js
@@ -3,104 +3,67 @@ import BadgeFilterSolid from "../../../components/badges/BadgeFilterSolid/BadgeF
 import CardContact from "../../../components/CardContact/CardContact";
 import InputSearchComplex from "../../../components/forms/inputs/InputSearchComplex/InputSearchComplex";
 import Pagination from "../../../components/pagination/Pagination/Pagination";
+import { getAllContact } from "../../../modules/apis/ContactAPI";
 
 import styles from "./ContactAutre.module.scss";
 
-const ContactAutre = () => (
-  <div className={styles.ContactAutre} data-testid="ContactAutre">
-    <div className={styles.ContactAutre_Search}>
-      <div className={styles.Search_Top}>
-        <InputSearchComplex />
+const ContactAutre = () => {
+  const [contacts, setContacts] = React.useState([]);
+
+  React.useEffect(() => {
+    getAllContact(
+      sessionStorage.getItem("token"),
+      (contactsFetched) => {
+        setContacts(contactsFetched);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }, []);
+
+  return (
+    <div className={styles.ContactAutre} data-testid="ContactAutre">
+      <div className={styles.ContactAutre_Search}>
+        <div className={styles.Search_Top}>
+          <InputSearchComplex />
+        </div>
+        <div className={styles.Search_Bottom}>
+          <BadgeFilterSolid
+            style={{ backgroundColor: "orange", cursor: "pointer" }}
+            title="Administrations"
+          />
+          <BadgeFilterSolid
+            style={{ backgroundColor: "violet", cursor: "pointer" }}
+            title="Intervenant"
+          />
+          <BadgeFilterSolid
+            style={{ backgroundColor: "green", cursor: "pointer" }}
+            title="Direction"
+          />
+        </div>
       </div>
-      <div className={styles.Search_Bottom}>
-        <BadgeFilterSolid
-          style={{ backgroundColor: "orange", cursor: "pointer" }}
-          title="Administrations"
-        />
-        <BadgeFilterSolid
-          style={{ backgroundColor: "violet", cursor: "pointer" }}
-          title="Intervenant"
-        />
-        <BadgeFilterSolid
-          style={{ backgroundColor: "green", cursor: "pointer" }}
-          title="Direction"
-        />
+
+      <div className={styles.Container_Grid}>
+        {contacts.map((contact, index) => (
+          <CardContact
+            name={contact.userInfo.name + " " + contact.userInfo.firstname}
+            role={contact.typeContactName}
+            email={contact.userInfo.email}
+            tel={contact.phone}
+            telLogo={contact.phone}
+            emailLogo={contact.userInfo.email}
+            key={index}
+          />
+        ))}
+      </div>
+
+      <div className={styles.ContactAutre_Pagination}>
+        <Pagination />
       </div>
     </div>
-
-    <div className={styles.Container_Grid}>
-      <CardContact
-        name="Carlos Ippo"
-        role="Directeur général"
-        email="carlos.ippo@gmail.com"
-        tel="+33 6 12 34 56 78"
-        telLogo="+33 6 12 34 56 78"
-        emailLogo="enzo.franceschinidev@gmail.com"
-      />
-      <CardContact
-        name="Carlos Ippo"
-        role="Directeur général"
-        email="carlos.ippo@gmail.com"
-        tel="+33 6 12 34 56 78"
-        telLogo="+33 6 12 34 56 78"
-        emailLogo="enzo.franceschinidev@gmail.com"
-      />
-      <CardContact
-        name="Carlos Ippo"
-        role="Directeur général"
-        email="carlos.ippo@gmail.com"
-        tel="+33 6 12 34 56 78"
-        telLogo="+33 6 12 34 56 78"
-        emailLogo="enzo.franceschinidev@gmail.com"
-      />
-      <CardContact
-        name="Carlos Ippo"
-        role="Directeur général"
-        email="carlos.ippo@gmail.com"
-        tel="+33 6 12 34 56 78"
-        telLogo="+33 6 12 34 56 78"
-        emailLogo="enzo.franceschinidev@gmail.com"
-      />
-      <CardContact
-        name="Carlos Ippo"
-        role="Directeur général"
-        email="carlos.ippo@gmail.com"
-        tel="+33 6 12 34 56 78"
-        telLogo="+33 6 12 34 56 78"
-        emailLogo="enzo.franceschinidev@gmail.com"
-      />
-      <CardContact
-        name="Carlos Ippo"
-        role="Directeur général"
-        email="carlos.ippo@gmail.com"
-        tel="+33 6 12 34 56 78"
-        telLogo="+33 6 12 34 56 78"
-        emailLogo="enzo.franceschinidev@gmail.com"
-      />
-      <CardContact
-        name="Carlos Ippo"
-        role="Directeur général"
-        email="carlos.ippo@gmail.com"
-        tel="+33 6 12 34 56 78"
-        telLogo="+33 6 12 34 56 78"
-        emailLogo="enzo.franceschinidev@gmail.com"
-      />
-      <CardContact
-        name="Carlos Ippo"
-        role="Directeur général"
-        email="carlos.ippo@gmail.com"
-        tel="+33 6 12 34 56 78"
-        telLogo="+33 6 12 34 56 78"
-        emailLogo="enzo.franceschinidev@gmail.com"
-      />
-    </div>
-
-    <div className={styles.ContactAutre_Pagination}>
-      <Pagination />
-    </div>
-  </div>
-);
-
+  );
+};
 ContactAutre.propTypes = {};
 
 ContactAutre.defaultProps = {};
